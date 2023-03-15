e0b38f7a6c0ac3e31cc8373c2557997c9d9999da
ndowkunda <ndowkunda@users.noreply.github.com>
2023-03-15 18:12:56 +0000

    Implement `scoreTeamA1` method

--- src/main/java/ScoreKeeper.java
+++ src/main/java/ScoreKeeper.java
@@ -5,4 +5,15 @@ public class ScoreKeeper {
         return score;
     }
 
+    public void scoreTeamA1() {
+        String scoreTeamA = score.substring(0, 3);
+        int scoreTeamAInt = Integer.parseInt(scoreTeamA);
+        int scoreLimit = 999;
+        if(scoreTeamAInt < scoreLimit) {
+            scoreTeamAInt++;
+        }
+        String scoreTeamAStr = String.format("%03d",scoreTeamAInt);
+        String scoreTeamBStr = score.substring(3);
+        score = scoreTeamAStr + scoreTeamBStr;
+    }
 }
--- src/test/java/ScoreKeeperTest.java
+++ src/test/java/ScoreKeeperTest.java
@@ -1,4 +1,6 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -10,6 +12,16 @@ class ScoreKeeperTest {
         String expectedScore = "000:000";
         assertThat(actualScore)
                 .isEqualTo(expectedScore);
+    }
+    @ParameterizedTest
+    @CsvSource({"1,001:000", "10,010:000", "100,100:000", "1000,999:000"})
+    void shouldIncreaseTeamAScoreByOneAfterTeamAScoresOnePoint(int increment, String expectedScore) {
+        ScoreKeeper scoreKeeper = new ScoreKeeper();
+        for (int i = 0; i < increment; i++) {
+            scoreKeeper.scoreTeamA1();
+        }
+        String actualScore = scoreKeeper.getScore();
+        assertThat(actualScore).isEqualTo(expectedScore);
     }
 
 }
