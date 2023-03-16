ea50a0d36ba986d9d46224a32db6d6b6cef90bb9
ndowkunda <ndowkunda@users.noreply.github.com>
2023-03-16 15:56:15 +0000

    refactor test cases
    Co-authored-by: NC-1234 <NC-1234@users.noreply.github.com>

--- test/java/ScoreKeeperTest.java
+++ test/java/ScoreKeeperTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ScoreKeeperTest {
+
     @Test
     void shouldReturnInitialScoreOfZero() {
         ScoreKeeper scoreKeeper = new ScoreKeeper();
@@ -13,63 +14,51 @@ class ScoreKeeperTest {
         assertThat(actualScore)
                 .isEqualTo(expectedScore);
     }
+
     @ParameterizedTest
     @CsvSource({"1,001:000", "10,010:000", "100,100:000", "1000,999:000"})
     void shouldIncreaseTeamAScoreByOneAfterTeamAScoresOnePoint(int increment, String expectedScore) {
-        ScoreKeeper scoreKeeper = new ScoreKeeper();
-        for (int i = 0; i < increment; i++) {
-            scoreKeeper.scoreTeamA1();
-        }
-        String actualScore = scoreKeeper.getScore();
-        assertThat(actualScore).isEqualTo(expectedScore);
+        testScoreIncrement("scoreTeamA1", increment, expectedScore);
     }
+
     @ParameterizedTest
     @CsvSource({"1,000:001", "10,000:010", "100,000:100", "1000,000:999"})
     void shouldIncreaseTeamBScoreByOneAfterTeamBScoresOnePoint(int increment, String expectedScore) {
-        ScoreKeeper scoreKeeper = new ScoreKeeper();
-        for (int i = 0; i < increment; i++) {
-            scoreKeeper.scoreTeamB1();
-        }
-        String actualScore = scoreKeeper.getScore();
-        assertThat(actualScore).isEqualTo(expectedScore);
+        testScoreIncrement("scoreTeamB1", increment, expectedScore);
     }
+
     @ParameterizedTest
     @CsvSource({"1,002:000", "10,020:000", "100,200:000", "500,998:000"})
     void shouldIncreaseTeamAScoreByTwoAfterTeamAScoresTwoPoints(int increment, String expectedScore) {
-        ScoreKeeper scoreKeeper = new ScoreKeeper();
-        for (int i = 0; i < increment; i++) {
-            scoreKeeper.scoreTeamA2();
-        }
-        String actualScore = scoreKeeper.getScore();
-        assertThat(actualScore).isEqualTo(expectedScore);
+        testScoreIncrement("scoreTeamA2", increment, expectedScore);
     }
+
     @ParameterizedTest
-    @CsvSource({"1,000:002, 10,000:020, 100,000:200, 500,000:998"})
+    @CsvSource({"1,000:002", "10,000:020", "100,000:200", "500,000:998"})
     void shouldIncreaseTeamBScoreByTwoAfterTeamBScoresTwoPoints(int increment, String expectedScore) {
-        ScoreKeeper scoreKeeper = new ScoreKeeper();
-        for (int i = 0; i < increment; i++) {
-            scoreKeeper.scoreTeamB2();
-        }
-        String actualScore = scoreKeeper.getScore();
-        assertThat(actualScore).isEqualTo(expectedScore);
+        testScoreIncrement("scoreTeamB2", increment, expectedScore);
     }
 
     @ParameterizedTest
     @CsvSource({"1,003:000", "10,030:000", "100,300:000", "500,999:000"})
     void shouldIncreaseTeamAScoreByThreeAfterTeamAScoresThreePoints(int increment, String expectedScore) {
-        ScoreKeeper scoreKeeper = new ScoreKeeper();
-        for (int i = 0; i < increment; i++) {
-            scoreKeeper.scoreTeamA3();
-        }
-        String actualScore = scoreKeeper.getScore();
-        assertThat(actualScore).isEqualTo(expectedScore);
+        testScoreIncrement("scoreTeamA3", increment, expectedScore);
     }
+
     @ParameterizedTest
-    @CsvSource({"1,000:003, 10,000:030, 100,000:300, 500,000:999"})
+    @CsvSource({"1,000:003", "10,000:030", "100,000:300", "500,000:999"})
     void shouldIncreaseTeamBScoreByThreeAfterTeamBScoresThreePoints(int increment, String expectedScore) {
+        testScoreIncrement("scoreTeamB3", increment, expectedScore);
+    }
+
+    private void testScoreIncrement(String scoreButton, int increment, String expectedScore) {
         ScoreKeeper scoreKeeper = new ScoreKeeper();
-        for (int i = 0; i < increment; i++) {
-            scoreKeeper.scoreTeamB3();
+        try {
+            for (int i = 0; i < increment; i++) {
+                ScoreKeeper.class.getMethod(scoreButton).invoke(scoreKeeper);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         String actualScore = scoreKeeper.getScore();
         assertThat(actualScore).isEqualTo(expectedScore);
